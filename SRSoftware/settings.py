38acf3fb9826bc0af725487369177dfbcf354ea9
microintelligence <microintelligence786@gmail.com>
2023-08-27 12:12:21 +0530

    initial commit

--- SRSoftware/settings.py
+++ SRSoftware/settings.py
@@ -78,12 +78,12 @@ WSGI_APPLICATION = 'SRSoftware.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 db_from_env = dj_database_url.config()
