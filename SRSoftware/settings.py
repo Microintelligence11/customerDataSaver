d3b0011e6cf5fef7af0c6be9bd69cc8f07565dd3
microintelligence <microintelligence786@gmail.com>
2023-08-27 15:16:45 +0530

    initial commite

--- SRSoftware/settings.py
+++ SRSoftware/settings.py
@@ -78,16 +78,20 @@ WSGI_APPLICATION = 'SRSoftware.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':'railway',
+        'USER':'postgres',
+        'PASSWORD':'VUzzDA8jlQSfSZTLrKDl',
+        'HOST':'containers-us-west-65.railway.app',
+        'PORT':'6161',
+}
+}
 
 
-# db_from_env = dj_database_url.config()
-# DATABASES['default'].update(db_from_env)
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
